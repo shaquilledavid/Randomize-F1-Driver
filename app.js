2f7a1b29ba2d47eb9cf81942ad319acac4592134
shaquille david <shaquilledavid@hotmail.com>
2021-07-10 13:39:10 -0400

    random generator working

--- app.js
+++ app.js
@@ -10,7 +10,28 @@ const app = Vue.createApp({
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
-    }
+    },
+
+    //define all our methods in here
+    methods: {
+        async getUser(){
+            //we want to get a random user everytime. we need to use fetch API with async await.
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+            //console.log(results);
+
+
+
+            
+            // we need this. to change the attributes
+
+            this.firstName = results[0].name.first,
+            this.lastName = results[0].name.last,
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.picture = results[0].picture.large
+        }, 
+    },
 
 })
 
